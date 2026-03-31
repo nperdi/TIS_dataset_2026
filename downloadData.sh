23020925eae3df7663b118b97e313b497b233630
nperdi <nikosp@uoa.gr>
2026-03-31 15:56:55 +0300

    ok

--- downloadData.sh
+++ downloadData.sh
@@ -12,3 +12,6 @@ grep -E '^chr([1-9]|1[0-9]|2[0-2]|X|Y)[[:space:]]' data/GENOME/hg38.chrom.size >
 wget -O data/GENOME/hg38.fa.gz https://hgdownload.gi.ucsc.edu/goldenPath/hg38/bigZips/latest/hg38.fa.gz
 gunzip data/GENOME/hg38.fa.gz
 samtools faidx data/GENOME/hg38.fa
+
+wget -O data/ensembl/Homo_sapiens.GRCh38.116.gtf.gz https://ftp.ensembl.org/pub/release-116/vertebrates/gtf/homo_sapiens/Homo_sapiens.GRCh38.116.gtf.gz
+wget -O data/ensembl/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz https://ftp.ensembl.org/pub/release-115/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz
